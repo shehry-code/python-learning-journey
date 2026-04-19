cb70c8f6c05f3d893577f5b26bd647ac2b60efcc
shehry <shehrimary@gmail.com>
2026-04-19 16:21:56 +0500

    _

--- string-concatination.py
+++ string-concatination.py
@@ -9,3 +9,23 @@ print ("My name and age is : ", full_name)
 
 name_and_age = f'My name is {full_name} and I"m {age} year old.'    #This f-sting stand for formatted string
 print (name_and_age)
+
+my_str = 'Hello world'
+print (my_str[0])       # This is string slicing we accessing it through index
+print (my_str[3])
+print (my_str[-1])      #It will start from the backside and print d
+
+print (my_str[1:4])     #Now we are slicing the string string[start:end]
+
+
+print(my_str[:7])  # Hello w
+print(my_str[8:])  # rld
+print(my_str)  # Hello world
+ 
+# string[start:stop:step]  there's also an optional step parameter, which is used to specify the increment between each index in the slice.
+
+print (my_str[0::2])
+
+#to reverse a string
+
+print (my_str[::-1])
---
+++ string_methods.py
@@ -0,0 +1,10 @@
+my_str = '       hello WORDL'
+
+print("Uppercase string : ", my_str.upper())       # The function upper() change the case of the string to uppercase
+
+print("Lowercase string : ", my_str.lower())        # The funciton lower() change the case of the string to lowercase
+
+print('after using strip ftn :', my_str.strip())     # The strip function will remove the white spaces from the string if no argument is pass to it
+
+print("relace string : ", my_str.replace('WORDL', 'world'))     # Replace function will repalce the part of the string with 2nd argument you send to it
+
